c6bb3c8211bee25f38fe0acf7100e960470bf4f5
freddy Santacruz <freddy.santacruzm@ucuenca.edu.ec>
2023-04-07 12:44:34 -0500

    primer commit

---
+++ backend/antawaSoft/src/main/java/com/qnksoft/antawasoft/AntawaSoftApplication.java
@@ -0,0 +1,13 @@
+package com.qnksoft.antawasoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntawaSoftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AntawaSoftApplication.class, args);
+    }
+
+}
---
+++ backend/antawaSoft/src/test/java/com/qnksoft/antawasoft/AntawaSoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qnksoft.antawasoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntawaSoftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
